afda64114cbff30195d113f8453f2360363fdea7
nicodau83 <nicolasdaunay@gmail.com>
2026-09-03 23:54:45 +0200

    Ajouter config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Renseigner ces deux valeurs après la création du projet Supabase.
+// La clé "anon" est conçue pour être publique. Ne jamais placer la clé "service_role" ici.
+window.FOILRACE_CONFIG = {
+  supabaseUrl: "",
+  supabaseAnonKey: ""
+};
