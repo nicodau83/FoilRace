ccbc0c70d39842f6a65b00e96d4f1703687142b3
nicodau83 <nicolasdaunay@gmail.com>
2026-09-04 01:34:27 +0200

    Actualise le cache PWA après correction Auth

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "foilrace-v5";
+const CACHE_NAME = "foilrace-v6";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./config.js", "./backend.js", "./app.js", "./account.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
