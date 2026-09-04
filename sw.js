2f6202eaf2064dfc9c53e4d9ffe83596b15e42b5
nicodau83 <nicolasdaunay@gmail.com>
2026-09-04 17:55:06 +0200

    Actualise le cache des photos du classement

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "foilrace-v15";
+const CACHE_NAME = "foilrace-v16";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./config.js", "./backend.js", "./app.js", "./account.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
