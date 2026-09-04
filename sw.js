7bae3731d7bbd11e26090bcd6737fa359f352f34
nicodau83 <nicolasdaunay@gmail.com>
2026-09-04 10:02:19 +0200

    Actualise le cache pour les mots de passe visibles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "foilrace-v14";
+const CACHE_NAME = "foilrace-v15";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./config.js", "./backend.js", "./app.js", "./account.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
