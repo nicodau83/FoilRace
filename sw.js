6d7402684fee452eb57963e19c54f525b231c80d
nicodau83 <nicolasdaunay@gmail.com>
2026-09-04 18:17:02 +0200

    Actualise le cache du nouveau classement

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "foilrace-v16";
+const CACHE_NAME = "foilrace-v17";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./config.js", "./backend.js", "./app.js", "./account.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
