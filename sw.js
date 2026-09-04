bb576dae7362b8746b783d731229627c14f98b31
nicodau83 <nicolasdaunay@gmail.com>
2026-09-04 08:07:10 +0200

    Actualise le cache après clarification de l'inscription

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "foilrace-v10";
+const CACHE_NAME = "foilrace-v11";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./config.js", "./backend.js", "./app.js", "./account.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
