53dab8f7a34b7cc85e3bb51138c43c0782c939be
nicodau83 <nicolasdaunay@gmail.com>
2026-09-04 02:00:05 +0200

    Actualise le cache après correction des formulaires

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "foilrace-v8";
+const CACHE_NAME = "foilrace-v9";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./config.js", "./backend.js", "./app.js", "./account.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
